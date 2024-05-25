9ef506da2d54d5798566e6d2df9376c154053dce
Ricssssssssss <ricapolido2001@gmail.com>
2024-05-25 20:59:32 +0800

    Add files via upload

---
+++ Student Evaluation Form/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Student_Evaluation_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            label3 = new Label();
+            radioButton6 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton8 = new RadioButton();
+            radioButton9 = new RadioButton();
+            radioButton10 = new RadioButton();
+            label4 = new Label();
+            radioButton11 = new RadioButton();
+            radioButton12 = new RadioButton();
+            radioButton13 = new RadioButton();
+            radioButton14 = new RadioButton();
+            radioButton15 = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(1220, 52);
+            label1.TabIndex = 0;
+            label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(400, 21);
+            label2.TabIndex = 1;
+            label2.Text = "1.  Do you always submit the tasks to ELMS?";
+            label2.Click += label2_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Cursor = Cursors.No;
+            radioButton1.Location = new Point(12, 166);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(83, 29);
+            radioButton1.TabIndex = 2;
+            radioButton1.Text = "Never\r\n";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(189, 166);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(85, 29);
+            radioButton2.TabIndex = 3;
+            radioButton2.Text = "Rarely\r\n";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(350, 166);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(126, 29);
+            radioButton3.TabIndex = 4;
+            radioButton3.Text = "Sometimes";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(577, 166);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(82, 29);
+            radioButton4.TabIndex = 5;
+            radioButton4.Text = "Often";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(762, 166);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(92, 29);
+            radioButton5.TabIndex = 6;
+            radioButton5.Text = "Always\r\n";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 240);
+            label3.Name = "label3";
+            label3.Size = new Size(560, 21);
+            label3.TabIndex = 7;
+            label3.Text = "2. Do you always get high points for your submission in ELMS?";
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(12, 302);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(83, 29);
+            radioButton6.TabIndex = 8;
+            radioButton6.Text = "Never\r\n";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(189, 302);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(85, 29);
+            radioButton7.TabIndex = 9;
+            radioButton7.Text = "Rarely\r\n";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(350, 302);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(126, 29);
+            radioButton8.TabIndex = 10;
+            radioButton8.Text = "Sometimes\r\n";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            radioButton9.AutoSize = true;
+            radioButton9.Location = new Point(577, 302);
+            radioButton9.Name = "radioButton9";
+            radioButton9.Size = new Size(82, 29);
+            radioButton9.TabIndex = 11;
+            radioButton9.Text = "Often";
+            radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            radioButton10.AutoSize = true;
+            radioButton10.Location = new Point(762, 302);
+            radioButton10.Name = "radioButton10";
+            radioButton10.Size = new Size(92, 29);
+            radioButton10.TabIndex = 12;
+            radioButton10.Text = "Always";
+            radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 377);
+            label4.Name = "label4";
+            label4.Size = new Size(464, 21);
+            label4.TabIndex = 13;
+            label4.Text = "3.  Is your GWA (General Weighted Averange) good?";
+            // 
+            // radioButton11
+            // 
+            radioButton11.AutoSize = true;
+            radioButton11.Location = new Point(12, 439);
+            radioButton11.Name = "radioButton11";
+            radioButton11.Size = new Size(115, 29);
+            radioButton11.TabIndex = 14;
+            radioButton11.Text = "Very poor";
+            radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12
+            // 
+            radioButton12.AutoSize = true;
+            radioButton12.Location = new Point(189, 439);
+            radioButton12.Name = "radioButton12";
+            radioButton12.Size = new Size(74, 29);
+            radioButton12.TabIndex = 15;
+            radioButton12.Text = "Poor";
+            radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton13
+            // 
+            radioButton13.AutoSize = true;
+            radioButton13.Location = new Point(350, 439);
+            radioButton13.Name = "radioButton13";
+            radioButton13.Size = new Size(124, 29);
+            radioButton13.TabIndex = 16;
+            radioButton13.Text = "Acceptable\r\n";
+            radioButton13.UseVisualStyleBackColor = true;
+            // 
+            // radioButton14
+            // 
+            radioButton14.AutoSize = true;
+            radioButton14.Location = new Point(577, 439);
+            radioButton14.Name = "radioButton14";
+            radioButton14.Size = new Size(82, 29);
+            radioButton14.TabIndex = 17;
+            radioButton14.Text = "Good";
+            radioButton14.UseVisualStyleBackColor = true;
+            // 
+            // radioButton15
+            // 
+            radioButton15.AutoSize = true;
+            radioButton15.Location = new Point(762, 439);
+            radioButton15.Name = "radioButton15";
+            radioButton15.Size = new Size(120, 29);
+            radioButton15.TabIndex = 18;
+            radioButton15.Text = "Very good";
+            radioButton15.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.MenuBar;
+            button1.Location = new Point(12, 529);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 19;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.MenuBar;
+            button2.Location = new Point(762, 529);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 20;
+            button2.Text = "Submit";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(1254, 583);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(radioButton15);
+            Controls.Add(radioButton14);
+            Controls.Add(radioButton13);
+            Controls.Add(radioButton12);
+            Controls.Add(radioButton11);
+            Controls.Add(label4);
+            Controls.Add(radioButton10);
+            Controls.Add(radioButton9);
+            Controls.Add(radioButton8);
+            Controls.Add(radioButton7);
+            Controls.Add(radioButton6);
+            Controls.Add(label3);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ControlText;
+            Name = "Form1";
+            Text = "Student Evaluation Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private Label label3;
+        private RadioButton radioButton6;
+        private RadioButton radioButton7;
+        private RadioButton radioButton8;
+        private RadioButton radioButton9;
+        private RadioButton radioButton10;
+        private Label label4;
+        private RadioButton radioButton11;
+        private RadioButton radioButton12;
+        private RadioButton radioButton13;
+        private RadioButton radioButton14;
+        private RadioButton radioButton15;
+        private Button button1;
+        private Button button2;
+        protected RadioButton radioButton1;
+    }
+}
---
+++ Student Evaluation Form/Form1.cs
@@ -0,0 +1,15 @@
+namespace Student_Evaluation_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
